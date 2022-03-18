579f97640338d19a420390148cfe2ae06250fb1e
vasanthi-dev <vasanthi2dev@gmail.com>
2022-03-18 06:14:14 -0400

    security group for instance

--- terraform-mutable/sg.tf
+++ terraform-mutable/sg.tf
@@ -16,7 +16,7 @@ resource "aws_security_group" "sg" {
     from_port        = 22
     to_port          = 22
     protocol         = "tcp"
-    cidr_blocks      = concat(data.terraform_remote_state.vpc.outputs.PRIVATE_SUBNET_CIDR, tolist([data.terraform_remote_state.vpc.outputs.VPC_CIDR]))
+    cidr_blocks      = concat(data.terraform_remote_state.vpc.outputs.PRIVATE_SUBNET_CIDR, tolist([data.terraform_remote_state.vpc.outputs.DEFAULT_VPC_CIDR]))
   }
 
   egress {
