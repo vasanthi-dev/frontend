14559df974449939993b0584f33157f3e8b5988e
vasanthi-dev <vasanthi2dev@gmail.com>
2022-03-18 10:27:09 -0400

    frontend

--- terraform-mutable/app-deploy.tf
+++ terraform-mutable/app-deploy.tf
@@ -3,13 +3,19 @@ resource "null_resource" "app-deploy" {
   provisioner "remote-exec" {
     connection {
       type     = "ssh"
-      user     = jsondecode(data.aws_secretsmanager_secret_version.secret.secret_string)["SSH_USERNAME"]
-      password = jsondecode(data.aws_secretsmanager_secret_version.secret.secret_string)["SSH_PASSWORD"]
+      user     = local.SSH_USERNAME
+      password = local.SSH_PASSWORD
       host     = aws_spot_instance_request.ec2-spot.*.private_ip[count.index]
     }
     inline = [
-     // "ansible-pull -i localhost, -u https://github.com/vasanthi-dev/ansible.git roboshop-pull.yml -e COMPONENT=${var.COMPONENT} -e ENV=${var.ENV} -e APP_VERSION=${var.APP_VERSION}"
-      "ansible-pull -U https://github.com/vasanthi-dev/ansible.git roboshop-pull.yml -e COMPONENT=catalogue -e ENV=dev -e APP_VERSION=${var.APP_VERSION} -e NEXUS_USERNAME=${nonsensitive(jsondecode(data.aws_secretsmanager_secret_version.secret.secret_string)["NEXUS_USERNAME"])} -e NEXUS_PASSWORD=${nonsensitive(jsondecode(data.aws_secretsmanager_secret_version.secret.secret_string)["NEXUS_PASSWORD"])} "
+      "ansible-pull -U https://github.com/vasanthi-dev/ansible.git roboshop-pull.yml -e COMPONENT=catalogue -e ENV=dev -e APP_VERSION=${var.APP_VERSION} -e NEXUS_USERNAME=${local.NEXUS_USERNAME} -e NEXUS_PASSWORD=${local.NEXUS_PASSWORD}"
     ]
   }
+}
+
+locals {
+  NEXUS_USERNAME = nonsensitive(jsondecode(data.aws_secretsmanager_secret_version.secret.secret_string)["NEXUS_USERNAME"])
+  NEXUS_PASSWORD = nonsensitive(jsondecode(data.aws_secretsmanager_secret_version.secret.secret_string)["NEXUS_PASSWORD"])
+  SSH_USERNAME = jsondecode(data.aws_secretsmanager_secret_version.secret.secret_string)["SSH_USERNAME"]
+  SSH_PASSWORD = jsondecode(data.aws_secretsmanager_secret_version.secret.secret_string)["SSH_PASSWORD"]
 }
